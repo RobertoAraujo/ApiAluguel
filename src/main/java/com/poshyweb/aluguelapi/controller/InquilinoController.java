e2c96e3e1609fa345562d84af8586279083645d9
Roberto Moraes <robertomoraes@lampp-it.com.br>
2022-02-19 13:43:31 -0500

    implementação de uma url para o cadastro

--- src/main/java/com/poshyweb/aluguelapi/controller/InquilinoController.java
+++ src/main/java/com/poshyweb/aluguelapi/controller/InquilinoController.java
@@ -1,61 +1,56 @@
 package com.poshyweb.aluguelapi.controller;
 
-import com.poshyweb.aluguelapi.dto.InquilinoDTO;
 import com.poshyweb.aluguelapi.model.InquilinoEntity;
 import com.poshyweb.aluguelapi.service.InquilinoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/v1")
+@RequestMapping("/v1")
 public class InquilinoController {
 
     @Autowired
     private InquilinoService service;
 
-    @GetMapping(value ="/listar")
-    public ResponseEntity<List<InquilinoEntity>> listarTodos(){
+    @GetMapping(value = "/listar")
+    public ResponseEntity<List<InquilinoEntity>> listarTodos() {
         return ResponseEntity.ok(service.findAll());
     }
 
-   @GetMapping(value = "/nome/{nome}")
-    public ResponseEntity<List<InquilinoEntity>> listarPorNome(@PathVariable String nome){
+    @GetMapping(value = "/nome/{nome}")
+    public ResponseEntity<List<InquilinoEntity>> listarPorNome(@PathVariable String nome) {
         return ResponseEntity.ok(service.findByNomeInquilino(nome));
-   }
+    }
 
-    @GetMapping(value ="id/{id}")
-    public ResponseEntity<Object> listarPorId(@PathVariable("id") long id){
+    @GetMapping(value = "id/{id}")
+    public ResponseEntity<Object> listarPorId(@PathVariable("id") long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
-    @PostMapping
+    @PostMapping(value = "/cadastrar")
     public ResponseEntity<InquilinoEntity> cadastrar(
-            @RequestBody InquilinoEntity inquilino, UriComponentsBuilder uriBilder){
+            @RequestBody InquilinoEntity inquilino, UriComponentsBuilder uriBilder) {
         service.save(inquilino);
         URI uri = uriBilder.path("/inquilino/{id}").buildAndExpand(inquilino.getId()).toUri();
-       return ResponseEntity.created(uri).body(
-               new InquilinoEntity(inquilino.getId(), inquilino.getNome(), inquilino.getCpf(),
-                       inquilino.getConjude(), inquilino.getQtdDependentes()));
+        return ResponseEntity.created(uri).body(
+                new InquilinoEntity(inquilino.getId(), inquilino.getNome(), inquilino.getCpf(),
+                        inquilino.getConjude(), inquilino.getQtdDependentes()));
     }
 
-    @PutMapping (value = "/update/{id}")
+    @PutMapping(value = "/update/{id}")
     public ResponseEntity<InquilinoEntity> update(@Validated @PathVariable Long id, @RequestBody InquilinoEntity inquilino) {
-        InquilinoEntity entity = service.update(id , inquilino);
+        InquilinoEntity entity = service.update(id, inquilino);
         return ResponseEntity.ok().body(entity);
     }
 
     @DeleteMapping(value = "/deletar/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<InquilinoEntity> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
